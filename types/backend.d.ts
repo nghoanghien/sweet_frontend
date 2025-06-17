fe4ca85da9ae71d819b2571a1e7d18da5276f1bf
TrinhHoa-23520508 <23520508@gm.uit.edu.vn>
2025-06-17 13:16:52 +0700

    res backend

--- types/backend.d.ts
+++ types/backend.d.ts
@@ -1,1 +1,7 @@
 //write all type backend here (entity or responseDTO which backend throw back)
+export interface IBackendRes<T> {
+    error?: string | string[];
+    message: string;
+    statusCode: number | string;
+    data?: T;
+  }
