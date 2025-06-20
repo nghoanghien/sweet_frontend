92a83e1afe9008a9d0f60b75521f310e981f310c
Hien <nguyenhoanghien.2505@gmail.com>
2025-06-20 19:33:49 +0700

    fix: get Address from get all customets api

--- src/services/customers.ts
+++ src/services/customers.ts
@@ -8,32 +8,6 @@ export const getAllCustomers = async () => {
   const response = await callGetAllKhachHang();
   console.log("Du lieu truoc khi mapping: ", response.data);
 
-  return response.data?.map((item: IKhachHangResDTO): User => {
-    const mockAddress: Address = {
-      addressID: 1,
-      houseNumber: '123',
-      streetName: "KTX khu B",
-      ward: 'Linh Xuân',
-      district: 'Thành phố Thủ Đức',
-      province: 'Thành phố Hồ Chí Minh',
-    }
-    return {
-      fullName: item.hoTen,
-      dateOfBirth: new Date(item.ngaySinh),
-      age: item.tuoi,
-      idCardNumber: item.cccd,
-      email: item.email,
-      phoneNumber: item.soDienThoai,
-      permanentAddress: mockAddress,
-      contactAddress: mockAddress,
-      role: mapApiToRole(item.vaiTro),
-      accountStatus: getAccountStatusByCode(item.trangThaiTaiKhoan.maTrangThai),
-
-      // Only for customer
-      customerID: item.khachHangID || undefined,
-      registrationDate: new Date(item.ngayDangKy) || null,
-      customerStatus: getCustomerStatusByCode(item.trangThaiKhachHang.maTrangThai),
-    }
-  }) || [];
+  return response.data?.map(mapApiToUser);
 }
 
