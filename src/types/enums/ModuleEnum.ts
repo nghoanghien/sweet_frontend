c71db1f6116cdc25335cda77bd3938aa78bc3f94
TrinhHoa-23520508 <23520508@gm.uit.edu.vn>
2025-06-21 01:54:30 +0700

    update type

---
+++ src/types/enums/ModuleEnum.ts
@@ -0,0 +1,17 @@
+export enum Module {
+  PhieuTraLai = 'PhieuTraLai',
+  PhieuGuiTien = 'PhieuGuiTien',
+  GiaoDich = 'GiaoDich',
+  TaiKhoanThanhToan = 'TaiKhoanThanhToan',
+  LichSuGiaoDich_TKTT = 'LichSuGiaoDich_TKTT',
+  QuyDinhLaiSuat = 'QuyDinhLaiSuat',
+  ChiTietQuyDinhLaiSuat = 'ChiTietQuyDinhLaiSuat',
+  DiaChi = 'DiaChi',
+  KhachHang = 'KhachHang',
+  LoaiTrangThai = 'LoaiTrangThai',
+  NhanVien = 'NhanVien',
+  QuyenHan = 'QuyenHan',
+  ThamSo = 'ThamSo',
+  TrangThai = 'TrangThai',
+  VaiTro = 'VaiTro'
+}
