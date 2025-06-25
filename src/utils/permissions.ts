586ba8e56d0459847f60d9560d853346b23ff32d
Hien <nguyenhoanghien.2505@gmail.com>
2025-06-26 00:49:08 +0700

    fix: get permissons function

--- src/utils/permissions.ts
+++ src/utils/permissions.ts
@@ -55,21 +55,83 @@ export const isValidPermission = (permission: string): permission is Permission 
 export const getActionCodes = (permission: Permission): number[] => {
   switch (permission) {
     case Permission.PAYMENT_ACCOUNT:
-      return [8];
+      return [4, 47, 57];
+      {/*
+        4. login
+        47. get giao dịch by tktt ID
+        57. get LSGD by tktt ID
+         */} 
     case Permission.SAVING_ACCOUNTS:
-      return [11];
+      return [11, 25, 62, 80, 81, 93];
+      {/*
+        11. Tạo phiếu rút tiền
+        25. Tạo mới phiếu gửi tiền
+        62. Lấy danh sách các phiếu rút tiền
+        80. Lấy phiếu gửi tiền by khách hàng ID
+        81. Lấy phiếu trả lãi theo phiếu gửi tiền ID
+        93. lấy lịch sử giao dịch phiếu gửi tiền theo ID
+         */} 
     case Permission.CUSTOMERS:
-      return [13];
+      return [11, 13, 15, 24, 25, 42, 43, 47, 49, 57, 62, 77, 80, 81, 90, 93];
+      {/*
+        11. Tạo phiếu rút tiền
+        13. get all khách hàng
+        15. vô hiệu hóa khách hàng
+        24. kích hoạt lại một khách hàng
+        25. Tạo mới phiếu gửi tiền
+        42. update khach hang
+        43. get khách hàng by ID
+        47. get giao dịch by tktt ID
+        49. tạo mới khách hàng
+        57. get LSGD by Tktt ID
+        62. lấy danh sách các phiếu rút tiền
+        77. Lấy tất cả phiếu gửi tiền
+        80. Lấy phiếu gửi tiền by khách hàng ID
+        81. Lấy phiếu trả lãi theo phiếu gửi tiền ID
+        90. update địa chỉ 
+        93. lấy lịch sử giao dịch phiếu gửi tiền theo ID
+         */} 
     case Permission.EMPLOYEES:
-      return [61];
+      return [41, 46, 55, 61, 83, 87, 88, 90];
+      {/*
+        41. get all nhan vien 
+        46. vô hiệu hóa nhân viên
+        55. kích hoạt nhân viên
+        61. get giao dịch by nhân viên
+        83. tạo mới nhân viên 
+        87. update nhân viên
+        88. get nhân viên by ID
+        90. update địa chỉ 
+         */} 
     case Permission.SAVING_PRODUCTS:
-      return [20, 64];
+      return [20, 22, 66, 67];
+      {/*
+        20. get all chi tiết quy định lãi suất
+        22. get all quy định lãi suất 
+        66. xóa quy định lãi suất
+        67. thêm quy định lãi suất
+         */} 
     case Permission.SALE_REPORTS:
-      return [4, 19];
+      return [100000];
+      {/*
+         */} 
     case Permission.SETTINGS:
-      return [35];
+      return [35, 36, 78];
+      {/*
+        35. update tham so by ID
+        36. get tham so by ID
+        78. Lấy tất cả tham số
+         */} 
     case Permission.PERMISSIONS:
-      return [33];
+      return [5, 21, 33, 34, 53, 73];
+      {/*
+        5. Tạo quyền hạn mới
+        21. Cấp quyền module to VaiTro
+        33. Xóa vai trò by ID
+        34. Get all vai tro
+        53. update vai tro by ID
+        73. Tạo vai trò mới 
+         */} 
     default:
       return [];
   }
